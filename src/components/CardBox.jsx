036dd06e58e6e0f8844e68ace2cded85842bed7f
Ethan <ethanhuang2078@gmail.com>
2024-04-20 08:30:02 -0700

    fix cardbox flexbox bugs

--- src/components/CardBox.jsx
+++ src/components/CardBox.jsx
@@ -1,31 +1,35 @@
 import React from "react";
 import Card from "./Card";
-import bodies "./astronomicalBodies";
+import bodies from "../utils/astronomicalBodies";
 
 const getSeason = (month) => {
-    if (month >= 3 && month <= 5) {
-      return "spring";
-    } else if (month >= 6 && month <= 8) {
-      return "summer";
-    } else if (month >= 9 && month <= 11) {
-      return "fall";
-    } else {
-      return "winter";
-    }
-  };
-  
-  const SideBar = () => {
-    const currentDate = new Date();
-    const currentMonth = currentDate.getMonth() + 1;
-    const currentSeason = getSeason(currentMonth);
-  
-    return (
-      <div className="w-1/3 bg-white h-screen flex justify-center">
-        <div className="flex flex-wrap w-[90%] h-[50%] my-5 gap-y-7 gap-10 overflow-y-auto">
-            {bodies[currentSeason].map((body, index) => 
-              <Card key={index} text={body} image={`constellations_pictures/${body}.webp`} />
-        )}
-        </div>
-      </div>
-    )
-  };
+  if (month >= 3 && month <= 5) {
+    return "spring";
+  } else if (month >= 6 && month <= 8) {
+    return "summer";
+  } else if (month >= 9 && month <= 11) {
+    return "fall";
+  } else {
+    return "winter";
+  }
+};
+
+const CardBox = () => {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentSeason = getSeason(currentMonth);
+
+  return (
+    <div className="flex flex-wrap w-[80%] h-[35%] my-5 gap-y-7 gap-10 overflow-y-auto border-2 border-gray-700 rounded-md justify-center py-7">
+      {bodies[currentSeason].map((body, index) => (
+        <Card
+          key={index}
+          text={body}
+          image={`constellations_pictures/${body}.webp`}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CardBox;
