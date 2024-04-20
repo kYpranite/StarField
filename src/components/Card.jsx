60e640e1bafc63d2a5ad01344c095f2361abba5f
Ethan <ethanhuang2078@gmail.com>
2024-04-20 08:33:03 -0700

    fix bug not showing image titles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Card = ({ title, image }) => {
+const Card = ({ text, image }) => {
   return (
     <div className="flex flex-col p-5 w-40 h-40  bg-gray-800 rounded-lg shadow-lg">
-      <h1 className="text-m text-white">{title}</h1> 
-      <img src={image} alt={title} className=" w-full h-full rounded-lg" />
+      <h1 className="text-m text-white">{text}</h1> 
+      <img src={image} alt={text} className=" w-full h-full rounded-lg" />
     </div>
   );
 };
