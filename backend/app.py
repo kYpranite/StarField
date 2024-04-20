8df8b4b4c91e5eb88306febe26a56454d3e7c92d
RonCodes88 <ronaldliyh@gmail.com>
2024-04-20 10:21:25 -0700

    added OpenWeatherAPI for backend

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ import google.generativeai as genai
 
 
 load_dotenv()
+open_weather_api_key=os.getenv("OPEN_WEATHER_API_KEY")
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 app = Flask(__name__)
@@ -21,6 +22,14 @@ def api():
 
     return jsonify({"response": response.text})
 
-
+@app.route("/api/weather")
+def weather():
+    latitude = 40.7128  # Example latitude (New York City)
+    longitude = -74.0060  # Example longitude (New York City)
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={open_weather_api_key}"
+    response = requests.get(url)
+    return jsonify({response: response.text})
+  
+    
 if __name__ == "__main__":
     app.run()
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Card = ({ text, image }) => {
   return (
-    <div className="flex flex-col p-5 w-40 h-40  bg-gray-800 rounded-lg shadow-lg">
+    <div className="flex flex-col items-center justify-center p-3 w-[45%] h-[50%]  bg-gray-800 rounded-lg shadow-lg">
       <h1 className="text-m text-white">{text}</h1> 
-      <img src={image} alt={text} className=" w-full h-full rounded-lg" />
+      <img src={image} alt={text} className=" w-[95%] h-[80%] rounded-lg" />
     </div>
   );
 };
