e92c10821d5ae966237a9a1eb809778534e82b58
RonCodes88 <ronaldliyh@gmail.com>
2024-04-20 15:46:17 -0700

    Added transitions for card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,7 @@ import React from "react";
 
 const Card = ({ text, image }) => {
   return (
-<<<<<<< Updated upstream
-    <div className="flex flex-col items-center justify-center p-3 w-30 h-40 bg-gray-800 rounded-lg shadow-lg">
-=======
     <button className="flex flex-col items-center justify-center p-3 w-[45%] h-[50%]  bg-gray-800 rounded-lg shadow-lg card cursor-pointer transition duration-300 ease-in-out transform hover:scale-105">
->>>>>>> Stashed changes
       <h1 className="text-m text-white">{text}</h1> 
       <img src={image} alt={text} className=" w-[95%] h-[80%] rounded-lg" />
     </button>
