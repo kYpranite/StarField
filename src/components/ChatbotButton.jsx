f57b26f3ab185e64efde47289cd59cd33e0a58f0
Ethan <ethanhuang2078@gmail.com>
2024-04-20 19:17:01 -0700

    add working chat system

---
+++ src/components/ChatbotButton.jsx
@@ -0,0 +1,23 @@
+export default function ChatbotButton({ handleButtonClick }) {
+  return (
+    <button
+      onClick={handleButtonClick}
+      className="fixed bottom-5 z-50 right-5 p-3 bg-gray-600 text-white rounded-full shadow-lg hover:bg-gray-700 transition duration-300 ease-in-out transform hover:scale-105"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
+    </button>
+  );
+}
